1817f08fd7ee9ff49c8b9344de463a16e161bdd7
samiha172 <samiha.akhter@northsouth.edu>
2022-09-23 21:49:02 +0600

    new changes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/contact-organizer',
+  publicPath: '/contact-organizer-vuejs',
   transpileDependencies: true
   
 })
